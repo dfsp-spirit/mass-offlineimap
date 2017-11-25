41aef5c78743a59f7f672ced724420b6fc321d6a
Tim Schäfer <ts+code@rcmd.org>
2017-11-26 00:04:34 +0100

    Create cfg file

--- mass-offlineimap.sh
+++ mass-offlineimap.sh
@@ -10,13 +10,21 @@ FILE_ACCOUNT_MAPPING='./account_mapping'
 echo "$APPTAG mass-offlineimap -- sync a number of mail accounts between an old and a new mail server"
 echo "$APPTAG Using old server data from '$FILE_OLDSERVER', new server data from '$FILE_NEWSERVER', account mapping data from '$FILE_ACCOUNT_MAPPING'."
 
+# The account mapping file has one line per account in format: '<old_user> <new_user> <old_pwd> <new_pwd>'. Separator is one space.
 while read -r line || [[ -n "$line" ]]; do
+    # Read account data from text file:
     echo "Text read from file: $line"
     OLD_USER=$(echo "$line" | awk '{print $1}')
     NEW_USER=$(echo "$line" | awk '{print $2}')
     OLD_PASSWORD=$(echo "$line" | awk '{print $3}')
     NEW_PASSWORD=$(echo "$line" | awk '{print $4}')
     echo "Found in line old user: $OLD_USER, new user $NEW_USER, old password $OLD_PASSWORD, new password $NEW_PASSWORD."
+    
+    # Now create the config file for this user:
+    FILE_USER_OFFLINEIMAP_CONF="offlineimap_${OLD_USER}"
+    echo "Creating config file $FILE_USER_OFFLINEIMAP_CONF for user $OLD_USER"
+    touch "$FILE_USER_OFFLINEIMAP_CONF"
+    echo '' > "$FILE_USER_OFFLINEIMAP_CONF"
 done < "$FILE_ACCOUNT_MAPPING"
 
 echo "$APPTAG Exiting."
