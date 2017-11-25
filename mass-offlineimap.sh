6b5ea046b103531fbe330b1b5ad5dd3f325ed25a
Tim Schäfer <ts+code@rcmd.org>
2017-11-25 23:52:45 +0100

    parse user data from file

--- mass-offlineimap.sh
+++ mass-offlineimap.sh
@@ -10,8 +10,13 @@ FILE_ACCOUNT_MAPPING='./account_mapping'
 echo "$APPTAG mass-offlineimap -- sync a number of mail accounts between an old and a new mail server"
 echo "$APPTAG Using old server data from '$FILE_OLDSERVER', new server data from '$FILE_NEWSERVER', account mapping data from '$FILE_ACCOUNT_MAPPING'."
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
+while read -r line || [[ -n "$line" ]]; do
     echo "Text read from file: $line"
+    OLD_USER=$(echo "$line" | awk '{print $1}')
+    NEW_USER=$(echo "$line" | awk '{print $2}')
+    OLD_PASSWORD=$(echo "$line" | awk '{print $3}')
+    NEW_PASSWORD=$(echo "$line" | awk '{print $4}')
+    echo "Found in line old user: $OLD_USER, new user $NEW_USER, old password $OLD_PASSWORD, new password $NEW_PASSWORD."
 done < "$FILE_ACCOUNT_MAPPING"
 
 echo "$APPTAG Exiting."
